f0f7c454a5e2b93382d2923cf8f9dc66b64da34e
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2025-08-10 09:10:10 -0300

    Update Exercicio03.java

--- src/atividadepratica1/Exercicio03.java
+++ src/atividadepratica1/Exercicio03.java
@@ -16,30 +16,32 @@ public class Exercicio03 {
         int menu;
 
         Scanner scan = new Scanner(System.in);
+        do {
+            System.out.print("Escolha entre: 1. Calcular área do quadrado ou 2. Calcular área do círculo: ");
+            menu = scan.nextInt();
 
-        System.out.print("Escolha entre: 1. Calcular área do quadrado ou 2. Calcular área do círculo: ");
-        menu = scan.nextInt();
+            switch (menu) {
+                case 1:
+                    System.out.print("Digite o comprimento de um dos lados do quadrado: ");
+                    lado = scan.nextDouble();
 
-        switch (menu) {
-            case 1:
-                System.out.print("Digite o comprimento de um dos lados do quadrado: ");
-                lado = scan.nextDouble();
+                    area = lado * lado;
+                    System.out.println("O valor da área do quadrado é " + area);
+                    break;
 
-                area = lado * lado;
-                System.out.println("O valor da área do quadrado é " + area);
-                break;
+                case 2:
+                    System.out.print("Digite o raio do circulo: ");
+                    raio = scan.nextDouble();
 
-            case 2:
-                System.out.print("Digite o raio do circulo: ");
-                raio = scan.nextDouble();
+                    area = (raio * raio) * pi;
+                    System.out.println("O valor da área do circulo é " + area);
+                    break;
 
-                area = (raio * raio) * pi;
-                System.out.println("O valor da área do circulo é " + area);
-                break;
+                default:
+                    System.out.println("ERRO! você não digitou uma das opções validas que é 1 ou 2");
+                    break;
+            }
+        } while (menu != 1 && menu != 2);
 
-            default:
-                System.out.println("ERRO! você não digitou uma das opções validas que é 1 ou 2");
-                break;
-        }
     }
 }
